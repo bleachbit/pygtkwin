1bb542a87743a2d8bd0014a3775c79d5a45541a3
Andrew Ziem <ahz001@gmail.com>
2026-08-26 08:35:49 -0600

    Increase oxipng optimization level from 4 to 6
    On local system, o4 rook 2.7 seconds and o6 took 4.4 seconds
    with gains of 5.7% and 5.9% bytes saved against baseline.
    
    `-o 6 --zopfli` improved 7% bytes against baseline at cost of
    712 seconds

--- make_gtk_theme.sh
+++ make_gtk_theme.sh
@@ -103,7 +103,7 @@ print_savings() {
 echo "==> Optimizing PNG files with oxipng"
 png_before=$(total_bytes gtk-themes '*.png')
 time find gtk-themes -type f -name '*.png' -print0 \
-    | xargs -r -0 oxipng -q -o 4 --strip safe --alpha
+    | xargs -r -0 oxipng -q -o 6 --strip safe --alpha
 png_after=$(total_bytes gtk-themes '*.png')
 print_savings "PNG" "$png_before" "$png_after"
 
